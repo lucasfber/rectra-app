d4316d82eff9481eba0866dbdc9fca3c20df59d7
Lucas Bertoldo <lucas.fber@gmail.com>
2020-04-12 01:11:51 -0300

    last update hour add to card

--- src/Card/index.jsx
+++ src/Card/index.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import "./style.scss"
 import globe from "../assets/img/globe.svg"
+import brazil from "../assets/img/brazil.png"
 import darkGlobe from "../assets/img/dark-globe.svg"
 
 const Card = ({ type }) => {
@@ -8,9 +9,9 @@ const Card = ({ type }) => {
     <div className={`card ${type}`}>
       <div className="card__inner">
         <img src={type ? darkGlobe : globe} alt="" />
-        <h2>1777517</h2>
+        <h2>404236</h2>
         <p className="covered">Pessoas curadas</p>
-        <p className="last-update">Última Atualização: 02/04/2020</p>
+        <p className="last-update">Última Atualização: 11/04/2020 | 22:27:34</p>
       </div>
     </div>
   )
